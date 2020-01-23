31724a153d9b49207b817cc84122402364ca1635
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-23 16:59:16 +0200

    Fixed imports

--- R/00.R
+++ R/00.R
@@ -7,7 +7,7 @@
 #' @importFrom glue     glue
 #' @importFrom readr    parse_integer
 #' @importFrom stringr  str_trim str_dup str_sub str_locate_all str_ends regex str_extract
-#' @importFrom stringr  str_match_all str_match str_detect str_pad str_split
+#' @importFrom stringr  str_match_all str_match str_detect str_pad str_split str_starts
 #' @importFrom purrr    %>% discard flatten_chr map_dfr map walk every compose map_chr imap
 #' @importFrom purrr    keep map_if map_int pmap_chr map2_chr
 #' @importFrom tibble   as_tibble tibble is_tibble
